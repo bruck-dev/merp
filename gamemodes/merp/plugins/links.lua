377bcf1f3044ed106ce79624d173180a2874eae1
bruck-dev <141885443+bruck-dev@users.noreply.github.com>
2024-06-24 20:54:50 -0500

    Add server links, update gamemode label
    1) Added links to the server content and discord with /content and /discord respectively.
    2) Changed the gamemode from "Mass Effect RP" to "masseffect" such that it shows up on the browser as "IX: Mass Effect".
    3) Added Firestorm flamethrower item.

--- gamemodes/merp/plugins/links.lua
+++ gamemodes/merp/plugins/links.lua
@@ -4,14 +4,14 @@ PLUGIN.author = "Ayreborne"
 PLUGIN.description = "Adds links to the various content"
 
 PLUGIN.urls = {
-	["Discord"] = "PLACEHOLDER",
-	["Content"] = "PLACEHOLDER",
+	["Discord"] = "https://discord.gg/De4A25U6vY",
+	["Content"] = "https://steamcommunity.com/sharedfiles/filedetails?id=3265880265",
 }
 
 -- Adding commands for URLs.
 for k,v in pairs(PLUGIN.urls) do
 	ix.command.Add(k, {
-		description = "Directs you to our " .. k .. ".",
+		description = "Directs you to our Server " .. k .. ".",
 		OnRun = function(self, client)
 			local lua = "gui.OpenURL('" .. v .. "')"
 			client:SendLua(lua)
---
+++ gamemodes/merp/schema/items/weapons/sh_firestorm.lua
@@ -0,0 +1,14 @@
+ITEM.name = "M-451 Firestorm"
+ITEM.description = "A short-ranged flamethrower, the Firestorm suppresses the regenerative capabilities of vorcha and krogan. Effective against armor, shields, and biotic barriers."
+ITEM.model = "models/mass_effect_3/weapons/heavy_weapons/m-451 firestorm.mdl"
+ITEM.class = "tfa_firestorm"
+ITEM.weaponCategory = "primary"
+ITEM.width = 3
+ITEM.height = 2
+ITEM.noBusiness = true
+
+ITEM.iconCam = {
+	ang	= Angle(0, 1, 0),
+	fov	= 10,
+	pos	= Vector(-200, 0, 0)
+}
